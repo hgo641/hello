c03966d87e91f3830f87da0354b785967cc46783
hgo641 <hgo641@gmail.com>
2020-07-23 15:29:02 +0900

    My first commit!

---
+++ Project1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	int a;
+	int b;
+	int c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << a + b + c << endl;
+
+
+
+
+
+
+
+
+
+	int d;
+	cin >> d;
+
+
+	return 0;
+}
